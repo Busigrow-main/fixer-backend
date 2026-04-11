72bec756551335295044e28aa81f73595da9c290
misanthropic-codes <airabhishek098@gmail.com>
2026-04-11 16:38:18 +0530

    Add gitignore and remove node_modules from tracking

--- dist/users/users.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'mongoose';
-import { User, UserDocument } from './schemas/user.schema';
-export declare class UsersService {
-    private userModel;
-    constructor(userModel: Model<UserDocument>);
-    findOneByPhone(phone: string): Promise<UserDocument | null>;
-    create(userData: Partial<User>): Promise<UserDocument>;
-}
